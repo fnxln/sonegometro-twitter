d10cb49e7c82b671d534f0e5674aa9d81c6c916f
Lin Diniz <gabrielvdiniz@gmail.com>
2020-12-04 20:00:06 -0300

    so numero

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import puppeteer from 'puppeteer';
 import os from 'os';
 import { createWorker  }  from 'tesseract.js';
-
-
+import moment from 'moment';
+moment.locale('pt-br');
+const hora = moment().format('LL'); 
+console.log(hora);
 
 (async () => {
     const browser = await puppeteer.launch(); 
@@ -11,10 +13,13 @@ import { createWorker  }  from 'tesseract.js';
         width: 700,
         height: 100
     })
-     
+    console.log('Entranndo no site')
     await page.goto('https://fnxln.github.io/ssonegometroapi/');
+    
+    console.log('Tirando Print')
     await page.screenshot({path: 'temp.jpg'});
-  
+
+    console.log('Fechando Puppeteer')
     await browser.close();
   
     
@@ -23,6 +28,8 @@ import { createWorker  }  from 'tesseract.js';
 const worker = createWorker();
 
 (async () => {
+
+  console.log('Abrindo Tesseract')
   await worker.load();
   await worker.loadLanguage('eng');
   await worker.initialize('eng');
@@ -30,6 +37,11 @@ const worker = createWorker();
     tessedit_char_whitelist: '0123456789',
   });
   const { data: { text } } = await worker.recognize('./temp.jpg');
-  console.log(text);
+
+  console.log('Dados:')
+  let linhas = text.split('\n');
+  linhas.splice(0,1);
+  let textofinal = linhas.join('\n')
+  console.log(textofinal);
   await worker.terminate();
 })();
