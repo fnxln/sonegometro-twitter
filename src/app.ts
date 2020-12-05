9556b052d2bbe79da676b62db89b16f9c0de5010
Lin Diniz <gabrielvdiniz@gmail.com>
2020-12-05 07:26:04 -0300

    yep

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,32 @@
 import puppeteer from 'puppeteer';
 import os from 'os';
+import fs from 'fs';
 import { createWorker  }  from 'tesseract.js';
 import moment from 'moment';
-import twitter from 'twitter';
 import dotenv from 'dotenv';
-
+import twit from 'twit';
+import Intl from 'intl';
+import { isElementAccessExpression } from 'typescript';
 //date stuff
 moment.locale('pt-br');
 const hora = moment().format('LL'); 
 console.log(hora);
 //twitter api
-let client = new twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  bearer_token: ''
+var T = new twit({
+  consumer_key:         process.env.consumer_key ?? '',
+  consumer_secret:      process.env.consumer_secret ?? '',
+  access_token:         process.env.access_token ?? '',
+  access_token_secret:  process.env.access_token_secret ?? '',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
 });
-
-
-
 
 
 
 
 // puppeteer stuff
 (async () => {
-    const browser = await puppeteer.launch(); 
+    const browser = await puppeteer.launch({args: ['--no-sandbox']}); 
     const page = await browser.newPage();
     await page.setViewport({
         width: 700,
@@ -59,10 +61,37 @@ const worker = createWorker();
   });
   const { data: { text } } = await worker.recognize('./temp.jpg');
 
-  console.log('Dados:')
+  
   let linhas = text.split('\n');
   linhas.splice(0,1);
   let textofinal = linhas.join('\n')
-  console.log(textofinal);
+  if (textofinal.length <= 12){
+    console.log('A porra do tesseract bugou o tamanho da string foi de: ' + textofinal.length + ' Caracteres, aguarde a proxima run')
+  }
+  if (textofinal.length >= 12){
+    console.log('Dados:');
+    console.log(textofinal);
+    let dindin = parseInt(textofinal);
+    var formatter = new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+
+  // These options are needed to round to whole numbers if that's what you want.
+  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
+
+ /* $2,500.00 */
+console.log(formatter.format(dindin));
+  //     T.post('statuses/update', { status: dindinstring }, function(err, data, response) {
+  // console.log(data);
+  
+  //     }
+  
+      // );
+  }
+  
+
+
   await worker.terminate();
 })();
